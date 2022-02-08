30f249e9acc13d551100955d7af52157b15d285f
Muralikrishna Shanmuganathan <shanmuganathan.m@northeastern.edu>
2022-02-08 07:20:15 -0500

    Adding dev files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Health Check endpoint - returns 200 HTTP status code
+app.get('/healthz', (req,res) => {
+    res.status(200).send();
+})
+
+const PORT = 5001;
+app.listen(PORT, () => console.log('Server listening on ${PORT}'))
+
+module.exports = app;
