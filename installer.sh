6aea67168777d0e658e1f67dac7791d81fd530b8
Muralikrishna Shanmuganathan <shanmuganathan.m@northeastern.edu>
2022-03-09 19:46:11 -0500

    Updates

--- installer.sh
+++ installer.sh
@@ -10,17 +10,18 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 #install latest version of node
 nvm install node
 
-#install mysql
+#install mysql client
 sudo yum update -y
 sudo wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 sudo rpm -Uvh mysql80-community-release-el7-3.noarch.rpm
-sudo yum install -y mysql-community-client
+sudo amazon-linux-extras install epel -y
 
 sleep 5
 
 #Install necessary dev tools
 sudo yum install -y gcc gcc-c++ make openssl-devel git
+
 
 #Install Node.js
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
