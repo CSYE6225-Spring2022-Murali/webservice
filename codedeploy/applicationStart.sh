f04a48d9afdd33f1da19edba8242977892d917fe
Muralikrishna Shanmuganathan <shanmuganathan.m@northeastern.edu>
2022-04-06 00:39:01 -0400

    changes

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/webservice
-sudo systemctl enable webservice
-sudo systemctl start webservice
+#sudo systemctl enable webservice
+#sudo systemctl start webservice
 sudo pm2 reload all --update-env
-sudo pm2 start index.js
+#sudo pm2 start index.js
 if [ $? == 0 ]; then echo "Application started successfully"; else echo "Something went wrong while starting the application"; fi
 sudo pm2 status
