4f590c4a59658176652e81ea1b109df8d69eded0
Muralikrishna Shanmuganathan <shanmuganathan.m@northeastern.edu>
2022-04-13 11:39:27 -0400

    removing config

--- config/dynamodb.config.js
+++ config/dynamodb.config.js
@@ -11,17 +11,17 @@ AWS.config.credentials = new AWS.EC2MetadataCredentials({
 
 const awsConfig = {
     "region" : "us-east-1",
-    "accessKeyId" : process.env.AWS_ACCESS_KEY_ID,
-    "secretAccessKey" : process.env.AWS_SECRET_ACCESS_KEY
+    // "accessKeyId" : process.env.AWS_ACCESS_KEY_ID,
+    // "secretAccessKey" : process.env.AWS_SECRET_ACCESS_KEY
 }
 
 AWS.config.update(awsConfig);
 
 const dynamoDBClient = new AWS.DynamoDB({
     // credentials: AWS.config.credentials,
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: "us-east-1",
+    // accessKeyId: process.env.AWS_ACCESS_KEY,
+    // secretAccessKey: process.env.AWS_SECRET_KEY,
+    // region: "us-east-1",
 });
 
 const dynamo = {};
